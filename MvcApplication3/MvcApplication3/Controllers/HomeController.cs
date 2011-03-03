c741a06ed9d7811fcb57b3c4a20586ad5c084868
i.enable.much@gmail.com <i.enable.much@gmail.com@863b3af1-06d7-f0dd-1b7e-d26225bb00a2>
2011-03-03 23:14:50 +0000

    Sync code

---
+++ MvcApplication3/MvcApplication3/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication3.Models;
+
+namespace MvcApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public JsonResult Create(Person p)
+        {
+            // use your ORM of choice here to persist your model 
+
+            // return feedback to jQuery
+            return Json(new { Message = "Your name is " + p.Lastname + ", " + p.Firstname });
+        }
+
+    }
+}
