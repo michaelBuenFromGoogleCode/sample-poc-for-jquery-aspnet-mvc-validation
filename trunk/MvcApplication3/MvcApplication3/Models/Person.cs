561f7021b6320ac5f676ced7ec331b50c05b29f6
i.enable.much@gmail.com <i.enable.much@gmail.com@863b3af1-06d7-f0dd-1b7e-d26225bb00a2>
2011-03-03 23:14:50 +0000

    Sync code

---
+++ trunk/MvcApplication3/MvcApplication3/Models/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcApplication3.Models
+{
+    public class Person
+    {
+        [Required(ErrorMessage="Last Name required")]
+        public string Lastname { get; set; }
+
+        [Required(ErrorMessage="First Name required")]
+        public string Firstname { get; set; }
+
+        [Required(ErrorMessage="Age Required")]
+        [Range(0,120, ErrorMessage="Age must be between 0 and 120")]
+        public int Age { get; set; }
+
+
+        // sourced here: http://www.regular-expressions.info/email.html
+        [Required(ErrorMessage="Email required")]
+        [RegularExpression(@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?",
+            ErrorMessage="Not a valid email")]
+        public string Email { get; set; }
+    
+    }
+}
